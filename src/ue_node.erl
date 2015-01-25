ae374e1ea5272c51c17be7a5f6d53dcde93570d7
VKI <lib.aca55a@gmail.com>
2015-01-25 18:12:38 +0200

    first commit

---
+++ src/ue_node.erl
@@ -0,0 +1,16 @@
+-module(ue_node).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    ue_node_sup:start_link().
+
+stop(_State) ->
+    ok.
